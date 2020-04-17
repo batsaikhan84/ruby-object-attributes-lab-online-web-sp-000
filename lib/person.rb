1fc9ee67eb72586a41a09377c8b4a2ee49921233
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-17 22:29:48 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+
+  def name=(person_name)
+    @this_person_name = name
+  end
+  def name
+    @this_person_name
+  end
+  
+  def job=(person_job)
+    @this_person_job = person_job
+  end
+  def job
+    @this_person_job
+  end
+
+end
